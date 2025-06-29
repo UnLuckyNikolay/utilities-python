1af4ff2ba0219e2b1cef5968c5a28bd5d9b9cb49
UnLuckyNikolay <nickslast@gmail.com>
2025-06-29 20:58:46 +0200

    Add Insertion sort

---
+++ sorting/insertion_sort.py
@@ -0,0 +1,40 @@
+from collections.abc import Callable, Iterable
+from typing import Any
+from numbers import Number
+
+
+def insertion_sort(iterable: Iterable, key: Callable[[Any], Number] = None, reverse: bool = False) -> list:
+    """
+    Sorts a copy of the iterable in place and retuns it as a list.
+
+    Pros: fast (for small lists), adaptive, stable.
+    
+    Parameters
+    ----------
+    iterable : Iterable
+        Iterable that needs to be sorted
+    key : func, optional
+        Function that returns key used for sorting
+    reverse : bool, optional
+        Set to True to sort from biggest to lowest
+
+    Returns
+    -------
+    list
+        Sorted *copy* of the iterable
+    """
+
+    iterable = list(iterable)
+
+    for i in range(1, len(iterable)):
+        j = i
+        while j>0:
+            left_val = iterable[j-1] if key == None else key(iterable[j-1])
+            right_val = iterable[j] if key == None else key(iterable[j])
+
+            if (not reverse and left_val <= right_val) or (reverse and left_val >= right_val):
+                break
+            iterable[j-1], iterable[j] = iterable[j], iterable[j-1]
+            j -= 1
+    
+    return iterable
--- sorting/test_sorting.py
+++ sorting/test_sorting.py
@@ -1,6 +1,7 @@
 import unittest
 from .bubble_sort import bubble_sort
 from .merge_sort import merge_sort
+from .insertion_sort import insertion_sort
 
 
 class Node():
@@ -18,18 +19,21 @@ class Node():
             self.val2 == node.val2)
 
 class TestSorting(unittest.TestCase):
-    # Lists to use for sorting
+    # Iterables to use for sorting
     nums = [1, 3, 2, 8, 13, 5, -400]
+    nums_tuple = (77, -3, 0, -18, 3, 16, 9)
     nodes = [Node(549), Node(69), Node(-1), Node(9001), Node(221)]
     nodes2 = [Node(549, 1), Node(-1), Node(69), Node(549, 2), Node(9001), Node(221), Node(549, 3)]
 
     # Sorted lists
     nums_sorted = [-400, 1, 2, 3, 5, 8, 13]
+    nums_tuple_sorted = [-18, -3, 0, 3, 9, 16, 77]
     nodes_sorted = [Node(-1), Node(69), Node(221), Node(549), Node(9001)]
     nodes2_sorted = [Node(-1), Node(69), Node(221), Node(549, 1), Node(549, 2), Node(549, 3), Node(9001)]
 
     # Reversed sorted lists
     nums_reverse = [13, 8, 5, 3, 2, 1, -400]
+    nodes_reverse = [Node(9001), Node(549), Node(221), Node(69), Node(-1)]
     nodes2_reverse = [Node(9001), Node(549, 1), Node(549, 2), Node(549, 3), Node(221), Node(69), Node(-1)]
 
 
@@ -38,6 +42,7 @@ class TestSorting(unittest.TestCase):
         self.assertEqual(self.nums, [1, 3, 2, 8, 13, 5, -400])
         self.assertEqual(self.nodes, [Node(549), Node(69), Node(-1), Node(9001), Node(221)])
         self.assertEqual(self.nodes2, [Node(549, 1), Node(-1), Node(69), Node(549, 2), Node(9001), Node(221), Node(549, 3)])
+        self.assertEqual(self.nums_tuple, (77, -3, 0, -18, 3, 16, 9))
 
     # Bubble sort
     def test__sort__bubble__numbers(self):
@@ -45,15 +50,25 @@ class TestSorting(unittest.TestCase):
         nums = bubble_sort(nums)
         self.assertEqual(nums, self.nums_sorted)
 
+    def test__sort__bubble__numbers_reverse(self):
+        nums = self.nums.copy()
+        nums = bubble_sort(nums, reverse=True)
+        self.assertEqual(nums, self.nums_reverse)
+
+    def test__sort__bubble__numbers_tuple(self):
+        nums = self.nums_tuple
+        nums = bubble_sort(nums)
+        self.assertEqual(nums, self.nums_tuple_sorted)
+
     def test__sort__bubble__nodes(self):
         nodes = self.nodes.copy()
         nodes = bubble_sort(nodes, key=lambda node: node.val)
         self.assertEqual(nodes, self.nodes_sorted)
 
-    def test__sort__bubble__reverse(self):
-        nums = self.nums.copy()
-        nums = bubble_sort(nums, reverse=True)
-        self.assertEqual(nums, self.nums_reverse)
+    def test__sort__bubble__nodes_reverse(self):
+        nodes = self.nodes.copy()
+        nodes = bubble_sort(nodes, key=lambda node: node.val, reverse=True)
+        self.assertEqual(nodes, self.nodes_reverse)
 
     # Merge sort
     def test__sort__merge__numbers(self):
@@ -61,15 +76,25 @@ class TestSorting(unittest.TestCase):
         nums = merge_sort(nums)
         self.assertEqual(nums, self.nums_sorted)
 
+    def test__sort__merge__numbers_reverse(self):
+        nums = self.nums.copy()
+        nums = merge_sort(nums, reverse=True)
+        self.assertEqual(nums, self.nums_reverse)
+
+    def test__sort__merge__numbers_tuple(self):
+        nums = self.nums_tuple
+        nums = merge_sort(nums)
+        self.assertEqual(nums, self.nums_tuple_sorted)
+
     def test__sort__merge__nodes(self):
         nodes = self.nodes.copy()
         nodes = merge_sort(nodes, key=lambda node: node.val)
         self.assertEqual(nodes, self.nodes_sorted)
 
-    def test__sort__merge__numbers_reverse(self):
-        nums = self.nums.copy()
-        nums = merge_sort(nums, reverse=True)
-        self.assertEqual(nums, self.nums_reverse)
+    def test__sort__merge__nodes_reverse(self):
+        nodes = self.nodes.copy()
+        nodes = merge_sort(nodes, key=lambda node: node.val, reverse=True)
+        self.assertEqual(nodes, self.nodes_reverse)
 
     def test__sort__merge__stability(self):
         nodes2 = self.nodes2.copy()
@@ -79,6 +104,42 @@ class TestSorting(unittest.TestCase):
     def test__sort__merge__stability_reverse(self):
         nodes2 = self.nodes2.copy()
         nodes2 = merge_sort(nodes2, key=lambda node: node.val, reverse=True)
+        self.assertEqual(nodes2, self.nodes2_reverse)
+
+    # Insertion sort
+    def test__sort__insertion__numbers(self):
+        nums = self.nums.copy()
+        nums = insertion_sort(nums)
+        self.assertEqual(nums, self.nums_sorted)
+
+    def test__sort__insertion__numbers_reverse(self):
+        nums = self.nums.copy()
+        nums = insertion_sort(nums, reverse=True)
+        self.assertEqual(nums, self.nums_reverse)
+
+    def test__sort__insertion__numbers_tuple(self):
+        nums = self.nums_tuple
+        nums = insertion_sort(nums)
+        self.assertEqual(nums, self.nums_tuple_sorted)
+
+    def test__sort__insertion__nodes(self):
+        nodes = self.nodes.copy()
+        nodes = insertion_sort(nodes, key=lambda node: node.val)
+        self.assertEqual(nodes, self.nodes_sorted)
+
+    def test__sort__insertion__nodes_reverse(self):
+        nodes = self.nodes.copy()
+        nodes = insertion_sort(nodes, key=lambda node: node.val, reverse=True)
+        self.assertEqual(nodes, self.nodes_reverse)
+
+    def test__sort__insertion__stability(self):
+        nodes2 = self.nodes2.copy()
+        nodes2 = insertion_sort(nodes2, key=lambda node: node.val)
+        self.assertEqual(nodes2, self.nodes2_sorted)
+
+    def test__sort__insertion__stability_reverse(self):
+        nodes2 = self.nodes2.copy()
+        nodes2 = insertion_sort(nodes2, key=lambda node: node.val, reverse=True)
         self.assertEqual(nodes2, self.nodes2_reverse)
 
     
