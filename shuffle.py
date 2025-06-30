b3d3d7638aaef60a383fb8e142757af7648db49a
UnLuckyNikolay <nickslast@gmail.com>
2025-06-30 02:59:54 +0200

    Add Shuffle function

---
+++ shuffle.py
@@ -0,0 +1,30 @@
+from collections.abc import Iterable
+from random import randint
+
+
+def shuffle(iterable: Iterable) -> list:
+    """
+    Shuffles a copy of the iterable in place and returns it as a list.
+
+    Uses Fisher-Yates shuffle (complexity - O(n)).
+
+    Parameters
+    ----------
+    iterable : Iterable
+        Iterable to be shuffled
+    
+    Returns
+    -------
+    list
+        A shuffled *copy* of the iterable
+    """
+
+    iterable = list(iterable)
+    i = len(iterable)
+
+    while i > 1:
+        i -= 1
+        rnd = randint(0, i)
+        iterable[i], iterable[rnd] = iterable[rnd], iterable[i]
+
+    return iterable
