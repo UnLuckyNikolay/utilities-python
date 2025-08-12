cd39c11f9589106c616168cf644eaf1f293c9724
UnLuckyNikolay <nickslast@gmail.com>
2025-08-12 22:49:28 +0200

    Add setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="utilities-python",
+    version="1.0.0",
+    packages=find_packages(),
+    author="UnLuckyNikolay",
+    description="A utility library of data structures and algorithms",
+    python_requires=">=3.10",
+)
